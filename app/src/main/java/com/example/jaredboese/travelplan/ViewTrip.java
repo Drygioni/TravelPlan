f598de4b7e68d033606f60253b0735fb0de8a8a8
Jared Boese <jgboese@mavs.coloradomesa.edu>
2017-12-07 00:20:16 -0700

    dataread

--- app/src/main/java/com/example/jaredboese/travelplan/ViewTrip.java
+++ app/src/main/java/com/example/jaredboese/travelplan/ViewTrip.java
@@ -1,16 +1,13 @@
 package com.example.jaredboese.travelplan;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-/**
- * Created by Jared Boese on 11/28/2017.
- */
-
-public class GoogleSignIn extends AppCompatActivity {
+public class ViewTrip extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_trip);
     }
 }
